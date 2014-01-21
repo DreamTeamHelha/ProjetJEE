047597b311a6ba1eb7969c9b5068813572ae76f1
oxydedefer <magnaito_25@hotmail.com>
2014-01-21 08:59:10 +0100

    derar

---
+++ pWebServiceBonjour/pWebServiceBonjour/src/com/helha/services/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.helha.services.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")//va definir uri de vos rssources
+public class ApplicationConfig extends Application{
+	
+	
+
+}
