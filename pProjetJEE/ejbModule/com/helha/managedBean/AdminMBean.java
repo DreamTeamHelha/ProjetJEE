602ed6d146c01c33d40c5949bfbff44ac55d42a8
oxydedefer <magnaito_25@hotmail.com>
2014-01-21 15:45:38 +0100

    Mbeean +admin Mden

--- pProjetJEE/ejbModule/com/helha/managedBean/AdminMBean.java
+++ pProjetJEE/ejbModule/com/helha/managedBean/AdminMBean.java
@@ -1,5 +1,110 @@
 package com.helha.managedBean;
 
-public class AdminMBean {
+import java.io.Serializable;
 
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.helha.ejb_interfaces.MemberEJBRemote;
+import com.helha.ejb_interfaces.ProductionEJBRemote;
+import com.helha.entities.Member;
+import com.helha.entities.Production;
+@Named
+@SessionScoped
+public class AdminMBean implements Serializable{
+
+	/*
+	 * creation user
+	 * remove user
+	 * update user information username+pwd
+	 * remove publication from user
+	 * 
+	 * 
+	 * update profil
+	 */
+	
+	private Member currentMember,newMember;
+	private Production production ;
+	@EJB
+	private MemberEJBRemote memberBean;
+	@EJB
+	private ProductionEJBRemote productionBean;
+	
+	public AdminMBean()
+	{
+		currentMember=new Member();
+		newMember=new Member();
+		production= new Production(); 
+	}
+
+	public boolean connection(String userName,String password)
+	{
+		boolean response=false;
+		for(Member m : memberBean.getMembers() )
+		{
+			if(m.getUsername().equals(userName)&&m.getPassword().equals(password))
+			{
+				currentMember=m;
+				response=true;
+			}
+		}
+		return response;
+	}
+	
+	public void createMember()
+	{
+		memberBean.addMember(newMember);
+	}
+	
+	public void removeMember()
+	{
+		memberBean.removeMember(newMember);
+	}
+	public void updateMember()
+	{
+		memberBean.updateMember(newMember);
+		
+	}
+	public void updatePersonnal()
+	{
+		memberBean.updateMember(currentMember);
+	}
+	
+	public void removeProduction(int workId)
+	{
+		newMember.removeWork(workId);
+		memberBean.updateMember(newMember);
+	}
+	public Member getCurrentMember() {
+		return currentMember;
+	}
+
+	public void setCurrentMember(Member currentMember) {
+		this.currentMember = currentMember;
+	}
+
+	public Production getProduction() {
+		return production;
+	}
+
+	public void setProduction(Production production) {
+		this.production = production;
+	}
+
+
+
+	public Member getNewMember() {
+		return newMember;
+	}
+
+
+
+	public void setNewMember(Member newMember) {
+		this.newMember = newMember;
+	}
+	
+	
+	
+	
 }
--- pProjetJEE/ejbModule/com/helha/managedBean/MemberMbean.java
+++ pProjetJEE/ejbModule/com/helha/managedBean/MemberMbean.java
@@ -5,9 +5,15 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.helha.ejb_interfaces.MemberEJBRemote;
+import com.helha.ejb_interfaces.ProductionEJBRemote;
+import com.helha.entities.Category;
 import com.helha.entities.Member;
+import com.helha.entities.Production;
 
 @Named
 @SessionScoped
@@ -19,19 +25,67 @@ public class MemberMbean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Member currentMember;
+
+	private Production production;
 	
 	@EJB
 	private MemberEJBRemote memberBean;
+	@EJB
+	private ProductionEJBRemote productionBean;
+
+	public MemberMbean(){
 	
-	
-	public MemberMbean(){}
-	
-	public void updateProfil()
-	{
-		
+		currentMember=new Member();
+		production=new Production();
 	}
-	public void connection()
-	{
 	
+	public void updateProfil( )
+	{
+		memberBean.updateMember(currentMember);
+	}
+	public boolean connection(String userName,String password)
+	{
+		boolean response=false;
+		for(Member m : memberBean.getMembers() )
+		{
+			if(m.getUsername().equals(userName)&&m.getPassword().equals(password))
+			{
+				currentMember=m;
+				response=true;
+			}
+		}
+		return response;
+	}
+
+	public void addProduction()
+	{
+		currentMember.addWork(production);
+		memberBean.updateMember(currentMember);
+	}
+	public void removeProduction(int workId)
+	{
+		currentMember.removeWork(workId);
+		memberBean.updateMember(currentMember);
+	}
+	public void updateProduction()
+	{
+		productionBean.updateProduction(production);
+	}
+	
+	public Member getCurrentMember() {
+		return currentMember;
+	}
+	
+	public void setCurrentMember(Member currentMember) {
+		this.currentMember = currentMember;
+	}
+	
+
+	public Production getProduction() {
+		return production;
+	}
+
+	public void setProduction(Production production) {
+		this.production = production;
 	}
 }
